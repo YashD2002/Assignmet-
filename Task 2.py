7a1f34272497138ca04c81dcb174fb8948570475
YashD2002 <136880720+YashD2002@users.noreply.github.com>
2025-04-06 12:14:20 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,7 @@
+p= input('Enter your first name: ')
+n=input('Enter your last name: ')
+p=(p)
+n=(n)
+
+M=(p+' '+n)
+print('Hello', M,'! Welcome to the Python program')
---
+++ Task1.py
@@ -0,0 +1,14 @@
+#Addition, Subtraction, Multiplication, Division
+p=input("Enter the first number: ")
+n= input("Enter the second number: ")
+p=int(p)
+n=int(n)
+a=(p + n)
+b=(p - n)
+c=(p*n)
+d=(p/n)
+
+print('Addition: ', a,)
+print('Subtraction: ' ,b)
+print('Multiplication: ', c)
+print('Division: ', d)
